12f7be11862beef1eb79f79b7006c4a51b684d2b
David Bastings <dbasting@f1r2s14.codam.nl>
2023-01-09 14:49:09 +0100

    Fixed some norm errors. Replaced all occurrences of signal() with sigaction() calls (wrapped nicely for convenience).

--- source/receive.c
+++ source/receive.c
@@ -1,59 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   receive.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dbasting <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/01/09 12:19:05 by dbasting      #+#    #+#                 */
+/*   Updated: 2023/01/09 14:32:15 by dbasting      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "comm.h"
 #include "byteops.h"
 #include "libft.h"
 #include "libftprintf.h"
-#include <limits.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-extern t_inbox	*inbox;
+extern t_inbox	*g_inbox;
 
 static void	catch_data(int signum);
 static void	catch_header(int signum);
+static void	set_handlers(void (*handler)(int));
 
 static void	catch_data(int signum)
 {
 	unsigned char	*byte;
 	unsigned char	mask;
-	
-	signal(signum, catch_data);
-	inbox->bits++;
-	byte = (unsigned char *)inbox->packet->data + ((inbox->bits - 1) / 8);
-	mask = (1 << (7 - (inbox->bits - 1) % 8));
+
+	g_inbox->bits++;
+	byte = (unsigned char *)g_inbox->packet->data + ((g_inbox->bits - 1) / 8);
+	mask = (1 << (7 - (g_inbox->bits - 1) % 8));
 	if (signum == SIGUSR2)
 		*byte |= mask;
-	if (inbox->bits == inbox->packet->header.len * 8)
+	if (g_inbox->bits == g_inbox->packet->header.len * 8)
 	{
 		inbox_process();
-		signal(SIGUSR1, catch_header);
-		signal(SIGUSR2, catch_header);
+		set_handlers(catch_header);
 	}
 }
 
 static void	catch_header(int signum)
 {
-	signal(signum, catch_header);
-	inbox->bits++;
-	bitshift_l(&(inbox->packet->header), sizeof(t_pheader));
+	t_pheader	*header;
+
+	header = &g_inbox->packet->header;
+	g_inbox->bits++;
+	bitshift_l(header, sizeof(t_pheader));
 	if (signum == SIGUSR2)
-		bitset_r(&(inbox->packet->header), sizeof(t_pheader), 1);	
-	if (inbox->bits == sizeof(t_pheader) * 8)
+		bitset_r(header, sizeof(t_pheader), 1);
+	if (g_inbox->bits == sizeof(t_pheader) * 8)
 	{
-		inbox->bits = 0;
-		signal(SIGUSR1, catch_data);
-		signal(SIGUSR2, catch_data);
-		inbox->packet->data = ft_calloc(inbox->packet->header.len + 1, sizeof(char));
-		if (inbox->packet->data == NULL)
+		set_handlers(catch_data);
+		g_inbox->bits = 0;
+		g_inbox->packet->data = ft_calloc(header->len + 1, sizeof(char));
+		if (g_inbox->packet->data == NULL)
 			exit(EXIT_FAILURE);
-		ft_printf("\nReceiving: %d chars from [%d].\n", inbox->packet->header.len, inbox->packet->header.origin);
 	}
+}
+
+static void	set_handlers(void (*handler)(int))
+{
+	struct sigaction	action;
+	struct sigaction	old_action;
+
+	action.sa_handler = handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+	sigaction(SIGUSR1, &action, &old_action);
+	sigaction(SIGUSR2, &action, &old_action);
 }
 
 void	receive(void)
 {
-	signal(SIGUSR1, catch_header);
-	signal(SIGUSR2, catch_header);
+	set_handlers(catch_header);
 	while (1)
 		pause();
 }
